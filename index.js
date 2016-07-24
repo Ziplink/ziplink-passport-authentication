1d5035c0e37cc4a56087455e47f64f2c99e3790e
aussieshibe <nnelson263@gmail.com>
2016-07-24 14:35:53 +0000

    Setup to require external configuration

--- index.js
+++ index.js
@@ -1,77 +1,73 @@
-var express = require('express');
-var router = express.Router();
 
-var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-var session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-
-var User = require('user-basic-mongo-storage');
-
-var config = require('./config.json');
-
-if(!config.CONFIGURED){
-  console.error ('*****************************');
-  console.error ('*          WARNING          *');
-  console.error ('* Configuration values for  *');
-  console.error ('*  authentication not set   *');
-  console.error ('*****************************');
-}
-
-passport.serializeUser(function(user, done) {
-    done(null, 
-    {
-      _id: user._id,
-      displayName: user.displayName
-    });
-});
-
-passport.deserializeUser(function(user, done) {
-    User.findOne({'_id':user._id}, function(err, user) {
-        done(err, user);
-    });
-});
-
-passport.use(new GoogleStrategy({  
-        clientID: config.GOOGLE_CLIENT_ID,
-        clientSecret: config.GOOGLE_CLIENT_SECRET,
-        callbackURL: config.GOOGLE_CALLBACK_URL
-    },
-    function(accessToken, refreshToken, profile, done) {
-      User.findByAuthentication(
-        {
-          'provider': profile.provider,
-          'ID': profile.id
-        }, function(err, user){
-          if(!user)
-            User.create({
-              displayName: profile.displayName,
-              authentication: {
-                provider: profile.provider,
-                ID: profile.id
-              }
-            }, function (err, user) {
-              return done(err, user);
-            });
-          else return done(err, user);
-        });
-         
-    }
-));
-
-router.use(session({
-  secret: 'supersecretstring',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false },
-  store: new MongoStore({ url: 'mongodb://localhost/ziplink' })
-}));
-
-router.use(passport.initialize());
-router.use(passport.session());
+module.exports = exports = function(authPath, config){
+  
+  if(!config)
+    throw new Error('Configuration not set');
+  
+  var express = require('express');
+  var router = express.Router();
+  
+  var passport = require('passport');
+  var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+  var session = require('express-session');
+  const MongoStore = require('connect-mongo')(session);
+  
+  var User = require('user-basic-mongo-storage');
+  
+  passport.serializeUser(function(user, done) {
+      done(null, 
+      {
+        _id: user._id,
+        displayName: user.displayName
+      });
+  });
+  
+  passport.deserializeUser(function(user, done) {
+      User.findOne({'_id':user._id}, function(err, user) {
+          done(err, user);
+      });
+  });
+  
+  passport.use(new GoogleStrategy({  
+          clientID: config.GOOGLE_CLIENT_ID,
+          clientSecret: config.GOOGLE_CLIENT_SECRET,
+          callbackURL: config.GOOGLE_CALLBACK_URL
+      },
+      function(accessToken, refreshToken, profile, done) {
+        User.findByAuthentication(
+          {
+            'provider': profile.provider,
+            'ID': profile.id
+          }, function(err, user){
+            if(!user)
+              User.create({
+                displayName: profile.displayName,
+                authentication: {
+                  provider: profile.provider,
+                  ID: profile.id
+                }
+              }, function (err, user) {
+                return done(err, user);
+              });
+            else return done(err, user);
+          });
+           
+      }
+  ));
+  
+  router.use(session({
+    secret: 'supersecretstring',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+    store: new MongoStore({ url: 'mongodb://localhost/ziplink' })
+  }));
+  
+  router.use(passport.initialize());
+  router.use(passport.session());
 
 
-module.exports = exports = function(authPath){
+
   router.get(authPath + '/google',
     passport.authenticate('google', { session: true, scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/userinfo.profile'] }));
     
